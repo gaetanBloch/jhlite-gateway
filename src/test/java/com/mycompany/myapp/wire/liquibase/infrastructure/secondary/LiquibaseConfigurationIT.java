55acc09f9fe5100d6198f04c025fb9b27163cf8d
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-12-08 08:31:39 +0100

    Apply liquibase module

---
+++ src/test/java/com/mycompany/myapp/wire/liquibase/infrastructure/secondary/LiquibaseConfigurationIT.java
@@ -0,0 +1,44 @@
+package com.mycompany.myapp.wire.liquibase.infrastructure.secondary;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import com.mycompany.myapp.IntegrationTest;
+
+@IntegrationTest
+class LiquibaseConfigurationIT {
+
+  @Nested
+  @IntegrationTest(properties = { "application.liquibase.async=true" })
+  class Async {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void shouldGetLiquibaseAsync() {
+      SpringLiquibase springLiquibase = (SpringLiquibase) applicationContext.getBean("liquibase");
+
+      assertThat(springLiquibase).isNotNull();
+    }
+  }
+
+  @Nested
+  @IntegrationTest(properties = { "application.liquibase.async=false" })
+  class Sync {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void shouldGetLiquibaseSync() {
+      SpringLiquibase springLiquibase = (SpringLiquibase) applicationContext.getBean("liquibase");
+
+      assertThat(springLiquibase).isNotNull();
+    }
+  }
+}
